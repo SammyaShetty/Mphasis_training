322c52c971c0ecc990590070519b6de658c7a899
Sammya Shetty <sammya.shetty@mphasis.com>
2026-02-27 15:50:19 +0530

    Spring  RestAPI CRUD

---
+++ SpringBootRestAPIMySQ/src/main/java/com/test/SpringBootRestApiMySqApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiMySqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiMySqApplication.class, args);
+	}
+
+}
