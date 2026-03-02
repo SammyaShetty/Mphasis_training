af1c8ae768f7ec58753185f67f62215927ee4211
Sammya Shetty <sammya.shetty@mphasis.com>
2026-03-02 17:56:22 +0530

    SpringBoot Security

---
+++ SpringBootSecurity/src/main/java/com/test/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.test.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableMethodSecurity
+public class UserController {
+
+	@GetMapping("/")
+  String homePage() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String loginPage() {
+		return "login";
+	}
+	
+	@GetMapping("/read")
+	@ResponseBody
+	public String userPage() {
+		return "USER AND ADMIN CAN READ THE DATA";
+	}
+	
+	@GetMapping("/delete")
+	@ResponseBody
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public String adminPage() {
+		return "ONLY ADMIN CAN DELETE THE DATA";
+	}
+	
+	@GetMapping("/update")
+	@ResponseBody
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public String updateUsers() {
+		return "DETAILS UPDATED WITH ADMIN ROLE";
+	}
+}
